21ae2e452476902810f692f5cfcfbccbc6edf50b
Henrik Heelweg <henrikhee@dhcp-10-29-233-193.dyn.MIT.EDU>
2025-02-10 16:09:36 -0500

    some updates

--- test/quantumHPC/gpu/DFT_gpu.py
+++ test/quantumHPC/gpu/DFT_gpu.py
@@ -31,7 +31,7 @@ def doDFT_gpu(molecule, basis = '6-31g', xc = 'b3lyp', density_fit = False, char
     mf.xc = xc
     mf.max_cycle = scf_cycles               
     mf.conv_tol = 1e-5
-    mf = mf.SMD()                           # TODO : look up this model
+    #mf = mf.SMD()                           # TODO : look up this model
     mf.with_solvent.method = 'DDCOSMO'      # COSMO implicit solvent model 
     #mf = solvent.ddCOSMO(mf).run()
     if density_fit:                         # optional: use density fit for accelerating computation
