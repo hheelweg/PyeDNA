9d7aab7dca203736f571be3f467b937b7648b989
Henrik Heelweg <henrikhee@Henriks-iMac.local>
2025-02-12 16:41:36 -0500

    some updates

--- test/quantumHPC/twomolecules/gpu/dft_control.py
+++ test/quantumHPC/twomolecules/gpu/dft_control.py
@@ -80,9 +80,8 @@ def main(molecules, time_steps, do_tddft):
         procs, mols = [], []
         for i, molecule_id in enumerate(molecules):
              
-            mol, conv = getMol(molecule_id, t)                                                               # create pyscf input for subprocess 
-            mols.append(mol)
-            dump(conv, f"input_{molecule_id}.joblib")
+            # create pyscf input for subprocess and store in cache
+            dump(getMol(molecule_id, t), f"input_{molecule_id}.joblib")
             procs.append(run_dft_tddft(molecule_id, t, gpu_id = i, do_tddft=do_tddft))                  # run processes
 
         # wait for both processes to finish and capture their outputs
@@ -95,12 +94,13 @@ def main(molecules, time_steps, do_tddft):
         # load and store relevant data from outputs
         exc, tdms, mols = [], [], []
         for i, molecule_id in enumerate(molecules):
+            # array-type data
             data = np.load(io.BytesIO(outputs[i]))
             exc.append(data["exc_energies"])
             tdms.append(data["tdms"])
-            # print('testtttttttt')
-            mol = load(f"mol_{molecule_id}.joblib")
-            mols.append(mol)
+            os.remove(f"output_{molecule_id}.npz")
+            # pyscf mol object
+            mols.append(load(f"mol_{molecule_id}.joblib"))
             os.remove(f"mol_{molecule_id}.joblib")
         
         # debug output of DFT/TDDFT
