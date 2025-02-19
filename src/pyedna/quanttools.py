1f1ae52d782daba083dd3cdcf07eed0221eb96d8
Henrik Heelweg <henrikhee@Henriks-iMac.local>
2025-02-19 16:41:00 -0500

    some updates

--- src/pyedna/quanttools.py
+++ src/pyedna/quanttools.py
@@ -373,6 +373,11 @@ def launchQMdriver(molecule_no, gpu_id):
     ORIGINAL_CWD = os.getcwd()
     print('testghgh', ORIGINAL_CWD)
 
+    from . import trajectory as traj
+    settings_dft, settings_tddft = traj.Trajectory.setQMSettings('qm.params')
+    print('settings', settings_dft)
+
+
     cmd = f"python {qm_driver_path} {molecule_no}"
     process = subprocess.Popen(cmd, env=env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)        
 
