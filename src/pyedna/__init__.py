acb51cf5774979d7b2f05aac7ef6121a36ebc8e1
Henrik Heelweg <henrikhee@Henriks-iMac.local>
2025-02-19 09:35:29 -0500

    some updates

--- src/pyedna/__init__.py
+++ src/pyedna/__init__.py
@@ -1,1 +1,8 @@
-# TODO : add this
+from const import *
+from fileproc import *
+from geomtools import *
+from quanttools import *
+from qm_driver import *
+from structure import *
+from trajectory import *
+from utils import *
--- src/pyedna/const.py
+++ src/pyedna/const.py
@@ -1,5 +1,6 @@
 # conversion for Bohr in Agström 
 BOHR = 0.529177249
+
 # energy conversion
 EH2EV = 27.2114         # Hartree -> eV
 EH2CM = 219474.6        # Hartree -> cm-1
