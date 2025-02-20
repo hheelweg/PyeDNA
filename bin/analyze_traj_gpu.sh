fd18c6887043832cd7697846cee3f1f3a3559ef4
Henrik Heelweg <henrikhee@dhcp-10-29-233-193.dyn.MIT.EDU>
2025-02-20 15:58:41 -0500

    some updates

---
+++ bin/analyze_traj_gpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=pyscf_gpu     # Job name
+#SBATCH --partition=gpu          # GPU partition
+#SBATCH --nodelist=gpu001        # Run on GPU node gpu001
+#SBATCH --gres=gpu:2             # Request 2 GPU
+#SBATCH --cpus-per-task=48       # Request 48 CPU cores
+#SBATCH --output=out_gpu.log     # Output file
+
+# Activate conda environment
+source activate AmberTools24
+
+# Add path to PyeDNA and define PyeDNA home
+export PYTHONPATH=$PYTHONPATH:/home/hheelweg/cy3cy5/PyeDNA/scripts
+export PYEDNA_HOME="/home/hheelweg/cy3cy5/PyeDNA"
+
+# Print allocated GPUs
+echo "Allocated GPUs: $CUDA_VISIBLE_DEVICES"
+
+# Run PySCF DFT calculation with GPU acceleration
+python -m analyze_traj
