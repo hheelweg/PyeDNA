10fa18059ba5d04af970352b3bd15045034490c0
Henrik Heelweg <henrikhee@dhcp-10-29-233-193.dyn.MIT.EDU>
2025-02-20 14:43:43 -0500

    some updates

--- src/pyedna/trajectory.py
+++ src/pyedna/trajectory.py
@@ -196,12 +196,10 @@ class Trajectory():
         # update default settings
         mols.update(user_mols) 
 
-        # store molecule IDs and make sure they are sorted
+        # store molecule IDs
         molecules = [value for key, value in mols.items() if key.startswith("molecule_") and value is not None]
-        molecules.sort(key=lambda x: int(x.split('_')[1]))
-        # store molecule names and make sure they are sorted
+        # store molecule names
         molecule_names = [value for key, value in mols.items() if key.startswith("name_") and value is not None]#.sort(key=lambda x: int(x.split('_')[1]))
-        molecule_names.sort(key=lambda x: int(x.split('_')[1]))
 
         # checkpoint
         assert(len(molecule_names) == len(molecules))
