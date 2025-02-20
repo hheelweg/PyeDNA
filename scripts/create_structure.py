a51312258c2035e3448b8164d1bf9afaa3417f52
Henrik Heelweg <henrikhee@dhcp-10-29-233-193.dyn.MIT.EDU>
2025-02-20 15:41:02 -0500

    some updates

---
+++ scripts/create_structure.py
@@ -0,0 +1,13 @@
+import pyedna 
+
+# TODO : make this a function that feeds in information about the molecules (dyes) and 
+# about the DNA sequence and returns a DNA+dye structure (maybe .pdb) and also the necessary
+# inputs for molecular dynamics. make this such that the orientation of dyes to DNA is correctly specified. 
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+
+    main()
