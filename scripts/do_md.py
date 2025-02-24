33c3de38a47bb3bafbf8abad891e16950678e134
Henrik Heelweg <henrikhee@Henriks-iMac.local>
2025-02-24 12:41:25 -0500

    some updates

--- scripts/do_md.py
+++ scripts/do_md.py
@@ -7,9 +7,10 @@ def main():
     
     # parse structure parameters
     dna_params = pyedna.CreateDNA.parseDNAStructure('struc.params')
+    print('dna params', dna_params)
     # parse input parameters for minimization/MD
     md_params = pyedna.MDSimulation.parseInputParams(dna_params = dna_params, file='md.params')
-    print('min_params', md_params) 
+    print('md_params', md_params) 
 
 
     # load MDSimulation object
