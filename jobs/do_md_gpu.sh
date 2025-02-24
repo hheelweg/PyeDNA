d84c354fae5f398ecd101cf14e5482c658e73163
Henrik Heelweg <henrikhee@dhcp-10-29-233-193.dyn.MIT.EDU>
2025-02-24 17:11:24 -0500

    some updates

--- jobs/do_md_gpu.sh
+++ jobs/do_md_gpu.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# dynamic job name
-#JOB_NAME=$(scontrol show job $SLURM_JOB_ID | awk -F= '/JobName/ {print $2}')
-JOB_NAME=${SLURM_JOB_NAME:-default_job}
-
 #SBATCH --nodes=1
-#SBATCH --partition=gpu                # GPU partition	
-#SBATCH --nodelist=gpu001              # Run on GPU node gpu001	
-#SBATCH --ntasks=1                     # # of tasks
-#SBATCH --gres=gpu:2                   # Request 2 GPU
-#SBATCH --cpus-per-task=8              # use 4-8 CPUs per GPU
-#SBATCH --job-name=${JOB_NAME}         # Use provided job name or "default_job" if none given
-#SBATCH --output=${JOB_NAME}.log       # Name output log file
+#SBATCH --partition=gpu                 # GPU partition	
+#SBATCH --nodelist=gpu001               # Run on GPU node gpu001	
+#SBATCH --ntasks=1                      # # of tasks
+#SBATCH --gres=gpu:2                    # Request 2 GPU
+#SBATCH --cpus-per-task=8               # use 4-8 CPUs per GPU
+#SBATCH --job-name=test                 # Use provided job name or "default_job" if none given
+#SBATCH --output=test.log               # Name output log file
 
 # USAGE:
 # sbatch this_script.sh --job-name=my_name
@@ -41,4 +37,4 @@ export LD_LIBRARY_PATH=$AMBERHOME/lib:$LD_LIBRARY_PATH
 # (3) production run
 
 # run python module for MD simulation
-python -m do_md > ${JOB_NAME}_output.log 2>&1
+python -m do_md > output.log 2>&1
