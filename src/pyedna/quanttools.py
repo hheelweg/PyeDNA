f0aa760b4e8320a6ffc6be213977fb31d1d2ab54
Henrik Heelweg <henrikhee@Henriks-MacBook-Pro.local>
2025-03-01 19:41:31 -0500

    some updates

--- src/pyedna/quanttools.py
+++ src/pyedna/quanttools.py
@@ -304,8 +304,10 @@ def doDFT_gpu(molecule, basis = '6-31g', xc = 'b3lyp',
     mf.xc = xc
     mf.max_cycle = scf_cycles               
     mf.conv_tol = 1e-9                      
-    mf = mf.SMD()                           # TODO : look up this model
-    mf.with_solvent.method = 'DDCOSMO'      # COSMO implicit solvent model 
+    # mf = mf.SMD()                           # TODO : look up this model
+    # mf.with_solvent.method = 'DDCOSMO'      # COSMO implicit solvent model 
+    mf = mf.PCM()
+    mf.with_solvent.method = 'COSMO'
     if density_fit:                         # optional: use density fit for accelerating computation
         mf.density_fit()
 
