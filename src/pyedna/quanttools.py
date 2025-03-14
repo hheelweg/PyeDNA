1d3bdb5cf187f5728153603a2e07b3e2e80baa72
Henrik Heelweg <hheelweg@mit.edu>
2025-03-14 17:45:44 -0400

    some updates

--- src/pyedna/quanttools.py
+++ src/pyedna/quanttools.py
@@ -87,7 +87,7 @@ def optimizeStructureFF_C2(moleculeNamePDB, out_file, stepsNo = 50000, econv = 1
                 second_C_vec (numpy.array): The reference vector used for classification.
                 axis_pair (tuple): Indices of the two atoms defining the axis.
             """
-            carbons, hydrogens = []
+            carbons, hydrogens = [], []
             
             # Extract atomic coordinates and classify atoms
             for i in range(1, mol.NumAtoms() + 1):
