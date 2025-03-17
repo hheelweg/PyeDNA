aa39f674c2f786309639f6ec3d4cb1473ca02609
Henrik Heelweg <hheelweg@mit.edu>
2025-03-17 18:21:12 -0400

    some updates

--- scripts/create_dye.py
+++ scripts/create_dye.py
@@ -25,13 +25,13 @@ def main():
     
     # (2) (optional) classical force-field optimization subject to point group symmetry of molecule
     # return .pdb of dye molecule with forcefield-optimized coordinates (with symmetry and constraint)
-    if symmetry_group is not None:
-        if symmetry_group == "C2":
-            pyedna.quanttools.optimizeStructureFFSymmetry(in_pdb_file = f"{dye_name}_ff.pdb", 
-                                                        out_pdb_file = f"{dye_name}_ff1.pdb",
-                                                        constraint = constraint, 
-                                                        point_group = symmetry_group
-                                                        )
+    # if symmetry_group is not None:
+    #     if symmetry_group == "C2":
+    #         pyedna.quanttools.optimizeStructureFFSymmetry(in_pdb_file = f"{dye_name}_ff.pdb", 
+    #                                                     out_pdb_file = f"{dye_name}_ff1.pdb",
+    #                                                     constraint = constraint, 
+    #                                                     point_group = symmetry_group
+    #                                                     )
 
     # (3) perform geometry optimization with DFT and return dye_name.pdb as geometry-optimized dye+linker file
     pyedna.quanttools.geometryOptimizationDFT_gpu(f"{dye_name}_ff1.pdb",
