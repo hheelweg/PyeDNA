1f3de0e1cfb4e93479b07393b2bb2a19a910ffe5
Henrik Heelweg <hheelweg@mit.edu>
2025-03-22 13:55:36 -0400

    some updates

--- src/pyedna/trajectory.py
+++ src/pyedna/trajectory.py
@@ -574,7 +574,7 @@ class Trajectory():
 
         # (1) define QM states we are interested in (0-indexed), i.e. (S_0^A , S_{stateB + 1}^B) <--> (S_{stateA + 1}^A, S_0^B)
         self.transitions = self.quant_info[0]["transitions"]
-        print(self.transitions)
+        print(self.transitions, self.num_molecules, flush = True)
         # check that length of each transition in self.transitions agrees with number of molecules
         for transition in self.transitions:
             assert(len(transition) == self.num_molecules)
