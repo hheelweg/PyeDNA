8a75e3d0777a30236ef6c3502c2c7a375e2843ea
Henrik Heelweg <hheelweg@mit.edu>
2025-04-22 14:08:16 -0400

    some updates

---
+++ jobs/vibrations.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --partition=normal              # nromal partition
+#SBATCH --cpus-per-task=48              # Request 48 CPU cores
+#SBATCH --job-name=orca                 # Job name
+#SBATCH --output=orca.log               # Output file
+
+# USAGE:
+# sbatch this_script.sh
+
+# Check if PYEDNA_HOME is set
+if [[ -z "$PYEDNA_HOME" ]]; then
+    echo "Error: PYEDNA_HOME is not set. Please set it in shell."
+    exit 1
+fi
+
+# Load config.sh from the root of PyeDNA to set user-specific environment variables
+CONFIG_FILE="$PYEDNA_HOME/config.sh"
+
+if [[ -f "$CONFIG_FILE" ]]; then
+    source "$CONFIG_FILE"
+else
+    echo "Error: Configuration file ($CONFIG_FILE) not found!"
+    exit 1
+fi
+
+
+# Force unbuffered output
+export PYTHONUNBUFFERED=1
+
+# Run ORCA vibrational analysis calculation
+python -m vibrational
