9e420954cb06ca338fff90addb274d74f142fd91
Henrik Heelweg <hheelweg@mit.edu>
2025-11-24 17:20:19 -0500

    modified mulliken analysis

--- src/pyedna/trajectory.py
+++ src/pyedna/trajectory.py
@@ -1187,12 +1187,12 @@ class Trajectory():
                                             do_fragments=self.molecule_do_fragments, 
                                             fragments=self.chromophores_fragments,
                                             charges=self.molecule_charges,
-                                            verbosity = 0
+                                            verbosity = 3
                                             )
                 else:
                     output_qm = qm.doQM_gpu(self.chromophores_conv, self.qm_outs,
                                             charges=self.molecule_charges, 
-                                            verbosity = 0
+                                            verbosity = 3
                                             )
                 # NOTE : set verbosity = 0 for production runs, and verbosity = 3 for debugging (STDOUT, STDERR). 
 
