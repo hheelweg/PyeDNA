da0bc6755959d604c463d66ff5eaf7dbe85a83b2
Henrik Heelweg <hheelweg@mit.edu>
2026-05-27 11:59:24 -0400

    quadrupole moments

--- src/pyedna/trajectory.py
+++ src/pyedna/trajectory.py
@@ -639,6 +639,7 @@ class Trajectory():
     # initialize output based on desired output parameters 
     def initOutput(self, output_length):
 
+        print(self.quant_info[0], flush=True)
 
         # ##############      (1)   Classical analysis                       ###############
 
