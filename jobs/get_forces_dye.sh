97db1b270a766bc3b21996d8bf9fd2f27542deb4
Henrik Heelweg <hheelweg@mit.edu>
2026-07-23 15:02:46 -0400

    some updates

--- jobs/get_forces_dye.sh
+++ jobs/get_forces_dye.sh
@@ -7,22 +7,20 @@
 #SBATCH --job-name=forces
 #SBATCH --output=slurm-%j.log
 
-Check if PYEDNA_HOME is set
+set -eo pipefail
 
-if [[ -z “$PYEDNA_HOME” ]]; then
-echo “Error: PYEDNA_HOME is not set. Please set it in shell.”
-exit 1
+if [[ -z "${PYEDNA_HOME:-}" ]]; then
+    echo "Error: PYEDNA_HOME is not set. Please set it in shell."
+    exit 1
 fi
 
-Load config.sh from the root of PyeDNA
+CONFIG_FILE="$PYEDNA_HOME/config.sh"
 
-CONFIG_FILE=”$PYEDNA_HOME/config.sh”
-
-if [[ -f “$CONFIG_FILE” ]]; then
-source “$CONFIG_FILE”
+if [[ -f "$CONFIG_FILE" ]]; then
+    source "$CONFIG_FILE"
 else
-echo “Error: Configuration file ($CONFIG_FILE) not found!”
-exit 1
+    echo "Error: Configuration file ($CONFIG_FILE) not found!"
+    exit 1
 fi
 
 python -m modify_prmtop
